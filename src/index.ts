4dd8dcb7addc0081f479efab084305418252dfde
Leshkinson <lopatkool93@gmail.com>
2022-07-26 18:48:18 +0300

    Fix yarn.lock, package.json, index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,27 @@
 import express from 'express';
 import {Request, Response} from "express";
+import TelegramApi from 'node-telegram-bot-api';
 import cors from 'cors';
 import axios from 'axios';
 import bodyParser from 'body-parser';
 import { config } from 'dotenv'
 
+
 config();
-const app = express();
-const PORT = process.env.PORT || 3000
-app.use(cors());
-app.use(bodyParser.text())
 
-const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`
+const bot = new TelegramApi(`${process.env.TELEGRAM_API_TOKEN}`, {polling: true})
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('I am alive!')
-})
-app.post('/new-message', async  (req: Request, res: Response) => {
-    console.log('Request.Body',req.body)
-    const message = req.body
-    console.log(message)
-    const messageText: string = message//?.text?.toLowerCase()?.trim();
-
-
-    if (!messageText) {
-        return res.status(400).send('an error occurs')
+bot.on('message', async msg => {
+    const text: string | undefined = msg.text;
+    const chatId: number = msg.chat.id;
+    if (text === '/start') {
+        await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/a6f/1ae/a6f1ae15-7c57-3212-8269-f1a0231ad8c2/1.webp');
+        await bot.sendMessage(chatId, `Привет ${msg.from?.username}`)
+    }
+    if (text === '/hello') {
+        await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/a6f/1ae/a6f1ae15-7c57-3212-8269-f1a0231ad8c2/27.webp');
+        await bot.sendMessage(chatId, `Этот бот, пока умеет только так, ${msg.from?.username}`)
     }
 
-    let responseText: string = 'Hello, It is my first message';
-
-    if (messageText === 'Oleg') {
-        try {
-            responseText = 'Hi, Oleg'
-            // await axios.post(TELEGRAM_URI, {
-            //     text: responseText
-            // })
-            res.send(responseText)
-            return
-        } catch (e) {
-            console.log(e)
-            res.send(e)
-        }
-    }
-
-    try {
-        await axios.post(TELEGRAM_URI, {
-            text: responseText
-        })
-        res.send('Done')
-    } catch (e) {
-        console.log(e)
-        res.send(e)
-    }
-})
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(msg)
 })
